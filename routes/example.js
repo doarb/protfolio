f216ab063358dc084e9720cbf4d85ee27596d5f9
doarb <damien.boucher.etu@univ-lemans.fr>
2024-04-02 11:04:10 +0200

    check commit

--- routes/example.js
+++ routes/example.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.route('/')
- .get((req, res) => {
-    res.status(200).json({ message: 'Hello World!' });
+router
+  .route("/")
+  .get((req, res) => {
+    res.status(200).json({ message: "Hello World!" });
   })
   .post((req, res) => {
-    res.status(200).json({ message: 'Hello World!' });
+    res.status(200).json({ message: "Hello World!" });
   })
   .delete((req, res) => {
-    res.status(200).json({ message: 'Hello World!' });
+    res.status(200).json({ message: "Hello World!" });
   });
-
 
 module.exports = router;
