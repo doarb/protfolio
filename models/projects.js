3f0fc22e154c94c22ef50137bd26091bb1b56243
doarb <damien.boucher.etu@univ-lemans.fr>
2024-04-15 11:51:05 +0200

    Correction pour mettre en binaire les fichiers

--- models/projects.js
+++ models/projects.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 let ProjectSchema = new mongoose.Schema({
   id: { type: Number, required: true, unique: true },
-  thumbnail: { type: String, required: true },
+  thumbnail: { type: Buffer, required: true },
   title: { type: String, required: true },
   descriptionIntro: { type: String, required: true, length: 80},
   descriptionMain: { type: String, required: true, length: 250},
   listWord: { type: Array, required: true, length: 10},
-  illutrations: { type: Array, required: true, length: 6},
+  illutrations: { type: [Buffer], required: true, length: 6},
   link: { type: String},
 });
 
