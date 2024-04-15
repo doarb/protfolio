cc3d51ef7ff4c11999a6d9bc8817668d5731cb48
doarb <damien.boucher.etu@univ-lemans.fr>
2024-04-15 09:29:34 +0200

    Mise en place du projet

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 const app = express();
-const testRouter = require('./routes/example');
+const testRouter = require("./routes/example");
 app.use(express.json());
 app.use(helmet());
 
-app.use('/test',testRouter)
+app.use("/api",testRouter);
 
 //recupere le port dans le fichier .env
-const PORT = process.env.SERVER_PORT;
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
